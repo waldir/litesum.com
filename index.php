8e2eaef9b95a0559a2d41f851d1a67ef12fee1ef
Jake Jarvis <jakejarvis@gmail.com>
2012-06-11 11:01:59 -0400

    phpfog

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   <meta name="description" content="Litesum instantly extracts brief summaries from Wikipedia on any given topic. Created by Jake Jarvis.">
   <meta name="keywords" content="wikipedia, wiki, encyclopedia, pedia, litesum, ajax, instant, jake jarvis, jakejarvis">
   <link rel="stylesheet" href="/style.css">
-  <script type="text/javascript" src="/article.js"></script>
+  <script src="/article.js"></script>
   <script>
     function sf() {
       document.f.q.focus();
@@ -30,31 +30,30 @@
     <p class="center">A brief summary of the requested topic will appear instantly.</p>
   </div>
 
-  <p class="small" style="margin-top:55px;"><span style="color:#e6a728;">Lite</span><span style="color:#356aa0;">sum</span> by <a href="http://www.jakejarvis.com/">Jake Jarvis</a>.</p>
-</div>
+  <p class="small" style="margin-top:55px;"><span style="color:#e6a728;">Lite</span><span style="color:#356aa0;">sum</span> by <a href="http://www.jakejarvis.com/">Jake Jarvis</a>.</p></div>
 
-<script type="text/javascript">
-  var _gauges = _gauges || [];
-  (function() {
-    var t   = document.createElement('script');
-    t.type  = 'text/javascript';
-    t.async = true;
-    t.id    = 'gauges-tracker';
-    t.setAttribute('data-site-id', '4fd6063c613f5d04b0000032');
-    t.src = '//secure.gaug.es/track.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(t, s);
-  })();
-</script>
+  <script>
+    var _gauges = _gauges || [];
+    (function() {
+      var t   = document.createElement('script');
+      t.type  = 'text/javascript';
+      t.async = true;
+      t.id    = 'gauges-tracker';
+      t.setAttribute('data-site-id', '4fd6063c613f5d04b0000032');
+      t.src = '//secure.gaug.es/track.js';
+      var s = document.getElementsByTagName('script')[0];
+      s.parentNode.insertBefore(t, s);
+    })();
+  </script>
 
-<script type="text/javascript">
-  var _gaq = [['_setAccount', 'UA-1563964-3'], ['_setDomainName', 'litesum.com'], ['_trackPageview']];
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-</script>
+  <script>
+    var _gaq = [['_setAccount', 'UA-1563964-3'], ['_setDomainName', 'litesum.com'], ['_trackPageview']];
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+  </script>
 
 </body>
 </html>
